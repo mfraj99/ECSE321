850b1df5ff9b4b525bb4ffc86dbddab71f83ad8d
Shi Tong Li <lishitongmy@gmail.com>
2020-02-08 14:53:05 -0500

    creating repository files in dao folder

---
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dao/PersonRepository.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.petshelter.dao;
+
+
+import org.springframework.data.repository.CrudRepository;
+import ca.mcgill.ecse321.petshelter.model.*;
+
+
+public interface PersonRepository extends CrudRepository<Person, String>{
+
+  Person findPersonByUsername(String username);
+
+}
--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dao/PetShelterRepository.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dao/PetShelterRepository.java
@@ -10,12 +10,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import ca.mcgill.ecse321.petshelter.model.User;
+import ca.mcgill.ecse321.petshelter.model.Person;
 
 @Repository
 public class PetShelterRepository {
 
   @Autowired
   EntityManager entityManager;
+  
+  @Transactional
+  public Person createPerson(String username, String password) {
+      Person p = new Person();
+      p.setUsername(username);
+      p.setPassword(password);
+      this.entityManager.persist(p);
+      return p;
+  }
+  
+  @Transactional
+  public Person getPerson(String username) {
+      Person p = this.entityManager.find(Person.class, username);
+      return p;
+  }
   
 }
