c300ee94e0aab1695c9e8ce667351193f9ea1b02
Xinrui Li <xinruili@hotmail.com>
2020-02-26 17:52:56 -0500

    Created the data transfer objects

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dto/DonationDto.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dto/DonationDto.java
@@ -1,5 +1,45 @@
 package ca.mcgill.ecse321.petshelter.dto;
 
-public class DonationDto {
+import java.util.Set;
 
+import ca.mcgill.ecse321.petshelter.model.Person;
+
+public class DonationDto {
+	private double amount;
+	private String comment;
+	private boolean setNameAnonymous;
+	private Set<PersonDto> person;
+	private Integer donationId;
+	
+	public DonationDto() {
+		
+	}
+	
+	public DonationDto(double amount, String comment, boolean setNameAnonymous, Set<PersonDto> person, Integer donationId) {
+		this.amount = amount;
+		this.comment = comment;
+		this.setNameAnonymous = setNameAnonymous;
+		this.person = person;
+		this.donationId = donationId;
+	}
+	
+	public double getAmount() {
+		return amount;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	
+	public boolean getSetNameAnonymous() {
+		return setNameAnonymous;
+	}
+	
+	public Set<PersonDto> getPerson() {
+		return person;
+	}
+	
+	public Integer getDonationId() {
+		return donationId;
+	}
 }
--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dto/QuestionDto.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/dto/QuestionDto.java
@@ -1,5 +1,45 @@
 package ca.mcgill.ecse321.petshelter.dto;
 
+import java.util.Set;
+
 public class QuestionDto {
+	private String question;
+	private String answer;
+	private Set<PetPostDto> isRelatedTo;
+	private Set<PersonDto> person;
+	private Integer questionId;
+	
+	public QuestionDto() {
+		
+	}
+	
+	
+	public QuestionDto(String question, String answer, Set<PetPostDto> isRelatedTo, Set<PersonDto> person, Integer questionId) {
+		this.question = question;
+		this.answer = answer;
+		this.isRelatedTo = isRelatedTo;
+		this.person = person;
+		this.questionId = questionId;
+	}
+
+	public String getQuestion() {
+		return this.question;
+	}
+
+	public String getAnswer() {
+		return this.answer;
+	}
+
+	public Set<PetPostDto> getIsRelatedTo() {
+		return this.isRelatedTo;
+	}
+
+	public Set<PersonDto> getPerson() {
+		return this.person;
+	}
+
+	public Integer getQuestionId() {
+		return this.questionId;
+	}
 
 }
