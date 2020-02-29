c0dc6d63ca507493ed8e6c3e5c9c2403d63f07f7
Michael Frajman <mhf420@gmail.com>
2020-02-29 14:03:05 -0500

    started controller for person

--- PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/PetShelterController.java
+++ PetShelter-Backend/src/main/java/ca/mcgill/ecse321/petshelter/controller/PetShelterController.java
@@ -2,8 +2,15 @@ package ca.mcgill.ecse321.petshelter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import ca.mcgill.ecse321.petshelter.dto.PersonDto;
+import ca.mcgill.ecse321.petshelter.model.Person;
 import ca.mcgill.ecse321.petshelter.service.PetShelterService;
 
 @CrossOrigin(origins = "*")
@@ -12,4 +19,39 @@ public class PetShelterController {
 
 	@Autowired
 	private PetShelterService service;
+	
+	//login person
+	
+	@PostMapping(value = {"/login/{tutorEmail}", "/login/{tutorEmail}/"})
+	public void tutorLogin(@PathVariable("tutorEmail") String tutorEmail,
+			@RequestParam String password) {
+		service.loginAsTutor(tutorEmail, password);
+	}
+	
+	@PutMapping(value = {"/logout", "/logout/"})
+	public void logout() {
+		service.logout();
+	}
+	
+	@GetMapping(value = {"/user", "/user/"})
+	public TutorDto getLoggedTutor() {
+		return converToDto((Tutor)service.getLoggedInUser());
+	}
+	
+	/*
+	@PostMapping(value = {"/persons/{username}/{password}", "/persons/{username}/{password}/"})
+	public PersonDto loginPerson(@PathVariable("username") String username, @PathVariable("password") String password) throws
+	IllegalArgumentException{
+		Person person = service.createPerson(username, password);
+		return convertToDto(person);
+	}
+	
+	private PersonDto convertToDto(Person p) {
+		if(p == null) {
+			throw new IllegalArgumentException("There is no such Person!");
+		}
+		PersonDto personDto = new PersonDto(p.getUsername(), p.getPassword());
+		return personDto;
+		
+	}*/
 }
